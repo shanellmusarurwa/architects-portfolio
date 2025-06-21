7db55b3d409918eb5d8e11ca8651d76ebd02de3d
Mitchelle <shanellmusarurwa6@gmail.com>
2025-06-21 03:03:36 +0200

    fixed an error

--- architect-portfolio/src/Pages/GalleryPage.jsx
+++ architect-portfolio/src/Pages/GalleryPage.jsx
@@ -12,48 +12,48 @@ const PhotoGallery = () => {
     },
     {
       id: 2,
-      src: './public/assets/Rectangle 23.png'
+      src: '/public/assets/Rectangle 23.png'
       
     },
     {
       id: 3,
-      src: './public/assets/Rectangle 24.png'
+      src: '/public/assets/Rectangle 24.png'
     
     },
     {
       id: 4,
-      src: './public/assets/Rectangle 25.png'
+      src: '/public/assets/Rectangle 25.png'
       
     },
     {
       id: 5,
-      src: './public/assets/Rectangle 26.png'
+      src: '/public/assets/Rectangle 26.png'
       
     },
     // Bottom row images (DEFEAL PROJECT)
     {
       id: 6,
-      src: './public/assets/Rectangle 27.png'
+      src: '/public/assets/Rectangle 27.png'
       
     },
     {
       id: 7,
-      src: './public/assets/Rectangle 28.png'
+      src: '/public/assets/Rectangle 28.png'
       
     },
     {
       id: 8,
-      src: './public/assets/Rectangle 29.png'
+      src: '/public/assets/Rectangle 29.png'
       
     },
     {
       id: 9,
-      src: './public/assets/Rectangle 30.png'
+      src: '/public/assets/Rectangle 30.png'
       
     },
     {
       id: 10,
-      src: './public/assets/Rectangle 31.png'
+      src: '/public/assets/Rectangle 31.png'
       
     }
   ];
--- architect-portfolio/src/Pages/LandingPage.jsx
+++ architect-portfolio/src/Pages/LandingPage.jsx
@@ -30,7 +30,7 @@ const LandingPage = () => {
         {/* Image */}
         < div className="hero-image">
           <img 
-            src="./public/assets/Rectangle 6.png" 
+            src="/public/assets/Rectangle 6.png" 
             alt="Project visual" 
           />
         </div>
@@ -45,13 +45,13 @@ const LandingPage = () => {
     {/* Left Column - Images */}
     <div className="images-column">
       <div className="small-image top">
-        <img src="./public/assets/Rectangle 9.png" alt="Architecture detail 1" />
+        <img src="/public/assets/Rectangle 9.png" alt="Architecture detail 1" />
       </div>
       <div className="small-image bottom">
-        <img src="./public/assets/Rectangle 10.png" alt="Architecture detail 2" />
+        <img src="/public/assets/Rectangle 10.png" alt="Architecture detail 2" />
       </div>
       <div className="large-image">
-        <img src="./public/assets/Rectangle 8.png" alt="Architecture overview" />
+        <img src="/public/assets/Rectangle 8.png" alt="Architecture overview" />
       </div>
     </div>
 
@@ -110,16 +110,16 @@ const LandingPage = () => {
         <div className="projects-grid">
           {/* Top row - 2 projects */}
           <div className="project-card top-row">
-            <img src="./public/assets/Rectangle 17.png" alt="Project 1" />
+            <img src="/public/assets/Rectangle 17.png" alt="Project 1" />
           </div>
           
           <div className="project-card top-row">
-            <img src="./public/assets/image 15.png" alt="Project 2" />
+            <img src="/public/assets/image 15.png" alt="Project 2" />
           </div>
           
           {/* Bottom row - 3 projects */}
           <div className="project-card">
-            <img src="./public/assets/image 16.png" alt="Project 3" />
+            <img src="/public/assets/image 16.png" alt="Project 3" />
           </div>
           
           <div className="project-card">
@@ -127,7 +127,7 @@ const LandingPage = () => {
           </div>
           
           <div className="project-card">
-            <img src="./public/assets/image 18.png" alt="Project 5" />
+            <img src="/public/assets/image 18.png" alt="Project 5" />
           </div>
         </div>
 
@@ -178,7 +178,7 @@ const LandingPage = () => {
       
       <div className="image-section">
         <img 
-          src="./public/assets/hassan.jpg" 
+          src="/public/assets/hassan.jpg" 
           alt="Contact illustration" 
           className="contact-image"
         />
